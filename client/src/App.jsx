7e7ad72cafb4eb4864c68015421cdda394175100
Niranjan Vasudevan <niranjanv233@gmail.com>
2026-03-04 14:01:05 +0800

    Complete base navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button, Navbar, NavbarLink, NavbarCollapse } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { RiSparkling2Line } from "react-icons/ri";
+
+export default function Header() {
+  return (
+    <Navbar className="border-b border-b-mist-300 flex flex-row items-center">
+      <div>
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <div className="flex flex-row items-center">
+            <img src="/axisvet.svg" alt="Axisvet Logo" className="h-7 sm:h-8" />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-row gap-10 text-sm font-semibold text-gray-600">
+      <Link to="/">Features</Link>
+      <Link to="/">Topics</Link>
+      <Link to="/">Pricing</Link>
+      </div>
+      <div className="flex flex-row gap-2">
+        <Link to="/sign-in">
+          <Button
+            className="bg-gray-100 text-black hover:text-black hover:bg-gray-200 hover:cursor-pointer font-semibold"
+            pill
+          >
+            Log In
+          </Button>
+        </Link>
+        <Button
+          className="text-white hover:cursor-pointer bg-[#2e8261] hover:bg-[#297355] gap-1"
+          pill
+        >
+          <RiSparkling2Line />
+          Get started
+        </Button>
+      </div>
+    </Navbar>
+  );
+}
