5e27a6773f717478f1ca432206189a0bc66a3f86
Niranjan Vasudevan <niranjanv233@gmail.com>
2026-03-12 12:43:58 +0800

    add redux toolkit and create user slice

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HeroUIProvider } from "@heroui/react";
 import "./index.css";
 import App from "./App.jsx";
+import { store } from "./redux/store.js";
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <HeroUIProvider>
       <App />
     </HeroUIProvider>
-  </StrictMode>,
+  </Provider>,
 );
