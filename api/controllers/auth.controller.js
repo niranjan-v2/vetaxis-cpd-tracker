1739c247a28ddcf44df16fbe66caa7af093ee98e
Niranjan Vasudevan <niranjanv233@gmail.com>
2026-03-13 16:17:38 +0800

    update jwt token expiry

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -44,23 +44,35 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.status(201).json({ message: "Signup successful" });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1m",
+    });
+    const { passwordHash: pass, ...rest } = newUser._doc;
+    res
+      .status(201)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
+
   if (!email || !password || email === "" || password === "") {
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
+
   try {
-    const validUser = await User.findOne({ email });
+    const validUser = await User.findOne({
+      $or: [{ email }, { username: email }],
+    });
     if (!validUser) {
-      next(errorHandler(404, "Invalid username or password"));
+      return next(errorHandler(404, "Invalid username or password"));
     }
+
     const validPassword = bcryptjs.compareSync(
       password,
       validUser.passwordHash,
@@ -68,18 +80,15 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "Invalid username or password"));
     }
-    const token = jwt.sign(
-      {
-        id: validUser._id,
-      },
-      process.env.JWT_SECRET,
-    );
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
     const { passwordHash: pass, ...rest } = validUser._doc;
     res
       .status(200)
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
+      .cookie("access_token", token, { httpOnly: true })
       .json(rest);
   } catch (error) {
     next(error);
