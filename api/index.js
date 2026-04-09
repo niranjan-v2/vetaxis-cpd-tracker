fd07b44e05154c2fe71126a5974b7b590762b8b2
Niranjan Vasudevan <niranjanv233@gmail.com>
2026-04-09 19:25:17 +0800

    add videoRoute

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import quizRoutes from "./routes/quiz.route.js";
+import videoRoute from "./routes/video.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -30,6 +31,7 @@ app.listen(PORT, () => {
 app.use('/api/user/', userRoutes);
 app.use('/api/auth/', authRoutes);
 app.use('/api/quiz/', quizRoutes);
+app.use("/api/videos", videoRoute);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
